df7662172896dcab8c1c45ec3bfe5e0b188947c8
Tagir <tagir@timalika.co.uk>
2016-11-29 08:03:08 +0000

    initial commit

---
+++ write-sqs-message.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/sqs"
+)
+
+func main() {
+	sess, err := session.NewSession()
+	if err != nil {
+		fmt.Println("failed to create session,", err)
+		return
+	}
+
+	svc := sqs.New(sess, &aws.Config{Region: aws.String("eu-west-1")})
+
+	params := &sqs.SendMessageInput{
+		MessageBody:  aws.String("a line or sequence of people or vehicles awaiting their turn to be attended to or to proceed."),
+		QueueUrl:     aws.String("https://sqs.eu-west-1.amazonaws.com/259764441114/Learn"),
+		DelaySeconds: aws.Int64(1),
+		MessageAttributes: map[string]*sqs.MessageAttributeValue{
+			"learn": {
+				DataType:    aws.String("String"),
+				StringValue: aws.String("queue"),
+			},
+		},
+	}
+	resp, err := svc.SendMessage(params)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	// Pretty-print the response data.
+	fmt.Println(resp)
+}
